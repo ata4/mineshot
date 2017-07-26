3cd7effbf63ef549c088eba567a422dbc29057b7
ata4 <barracuda415@yahoo.de>
2017-07-26 23:46:12 +0200

    Merged MineshotConfigGui with MineshotConfigGuiFactory, thanks to the improved interface

--- src/main/java/info/ata4/minecraft/mineshot/client/config/MineshotConfigGuiFactory.java
+++ src/main/java/info/ata4/minecraft/mineshot/client/config/MineshotConfigGuiFactory.java
@@ -9,10 +9,19 @@
  */
 package info.ata4.minecraft.mineshot.client.config;
 
+import info.ata4.minecraft.mineshot.Mineshot;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigCategory;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.client.IModGuiFactory;
+import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.IConfigElement;
 
 /**
  *
@@ -26,7 +35,7 @@ public class MineshotConfigGuiFactory implements IModGuiFactory {
 
     @Override
     public Class<? extends GuiScreen> mainConfigGuiClass() {
-        return MineshotConfigGui.class;
+        return GuiConfig.class;
     }
 
     @Override
@@ -48,7 +57,27 @@ public class MineshotConfigGuiFactory implements IModGuiFactory {
 
     @Override
     public GuiScreen createConfigGui(GuiScreen gs) {
-        return new MineshotConfigGui(gs);
+        Configuration config = Mineshot.instance.getConfigForge();
+        
+        // map config elements to their categories, except for CATEGORY_GENERAL
+        List<IConfigElement> configElems = config.getCategoryNames().stream()
+            .filter(catName -> !catName.equals(Configuration.CATEGORY_GENERAL))
+            .map(catName -> new ConfigElement(config.getCategory(catName)))
+            .collect(Collectors.toList());
+
+        // add props in CATEGORY_GENERAL directly to the root of the list
+        if (config.hasCategory(Configuration.CATEGORY_GENERAL)) {
+            ConfigCategory catGeneral = config.getCategory(Configuration.CATEGORY_GENERAL);
+            List<Property> props = catGeneral.getOrderedValues();            
+            configElems.addAll(props.stream()
+                .map(prop -> new ConfigElement(prop))
+                .collect(Collectors.toList())
+            );
+        }
+        
+        String title = GuiConfig.getAbridgedConfigPath(Mineshot.instance.getConfigForge().toString());
+        
+        return new GuiConfig(gs, configElems, Mineshot.ID, false, false, title);
     }
     
 }
