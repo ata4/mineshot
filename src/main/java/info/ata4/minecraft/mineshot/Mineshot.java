b49511e7854217eca4c3784ef5b6c14a081ac167
Fuzss <Henning@5xLehmann.de>
2018-06-26 14:34:09 +0200

    reverted

--- src/main/java/info/ata4/minecraft/mineshot/Mineshot.java
+++ src/main/java/info/ata4/minecraft/mineshot/Mineshot.java
@@ -38,7 +38,7 @@ public class Mineshot {
     
     public static final String NAME = "Mineshot";
     public static final String ID = "mineshot";
-    public static final String VERSION = "1.73";
+    public static final String VERSION = "@VERSION@";
     
     @Instance(ID)
     public static Mineshot instance;
